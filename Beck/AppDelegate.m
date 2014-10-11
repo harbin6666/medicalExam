43ae771257f79c200f9b168596ce7b1ae60e929e
Jiang li <jiangli2@yhd.com>
2014-10-11 12:21:39 +0800

    add cordite

--- Beck/AppDelegate.m
+++ Beck/AppDelegate.m
@@ -8,12 +8,13 @@
 
 #import "AppDelegate.h"
 
+#import "Item.h"
+
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
@@ -34,6 +35,66 @@
     else {
         [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
     }
+    
+    
+//    {
+//        News *news = [NSEntityDescription insertNewObjectForEntityForName:@"Item" inManagedObjectContext:self.managedObjectContext];
+
+//        news.title = @"今日头条";
+//        news.date = [NSDate date];
+//        news.count = @20;
+//    }
+//
+//    {
+//        NSError *error = nil;
+//        BOOL isSave =   [self.managedObjectContext save:&error];
+//        if (!isSave) {
+//            NSLog(@"error:%@,%@",error,[error userInfo]);
+//        }
+//        else{
+//            NSLog(@"保存成功");
+//        }
+//    }
+    
+//    {
+//        //创建取回数据请求
+//        NSFetchRequest *request = [[NSFetchRequest alloc] init];
+//        //设置要检索哪种类型的实体对象
+//        NSEntityDescription *entity = [NSEntityDescription entityForName:@"Item"inManagedObjectContext:self.managedObjectContext];
+//        //设置请求实体
+//        [request setEntity:entity];
+//        //指定对结果的排序方式
+//        NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name"ascending:NO];
+//        NSArray *sortDescriptions = [[NSArray alloc]initWithObjects:sortDescriptor, nil];
+//        [request setSortDescriptors:sortDescriptions];
+//        NSError *error = nil;
+//        //执行获取数据请求，返回数组
+//        NSMutableArray *mutableFetchResult = [[self.managedObjectContext executeFetchRequest:request error:&error] mutableCopy];
+//        if (mutableFetchResult == nil) {
+//            NSLog(@"Error: %@,%@",error,[error userInfo]);
+//        }
+//        
+//        NSLog(@"%@",mutableFetchResult);
+//    }
+//    
+//    NSLog(@"\n");
+//    
+//    {
+//        //创建取回数据请求
+//        NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Item"];
+//        NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];
+//        NSArray *sortDescriptions = [[NSArray alloc]initWithObjects:sortDescriptor, nil];
+//        [request setSortDescriptors:sortDescriptions];
+//        
+//        NSError *error = nil;
+//        //执行获取数据请求，返回数组
+//        NSMutableArray *mutableFetchResult = [[self.managedObjectContext executeFetchRequest:request error:&error] mutableCopy];
+//        if (mutableFetchResult == nil) {
+//            NSLog(@"Error: %@,%@",error,[error userInfo]);
+//        }
+//        
+//        NSLog(@"%@",mutableFetchResult);
+//    }
     
     return YES;
 }
@@ -58,6 +119,88 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Saves changes in the application's managed object context before the application terminates.
+    [self saveContext];
+}
+
+#pragma mark - Core Data stack
+
+@synthesize managedObjectContext = _managedObjectContext;
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory {
+    // The directory the application uses to store the Core Data store file. This code uses a directory named "com.yihaodian.onethestore.TestCoreData" in the application's documents directory.
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
+- (NSManagedObjectModel *)managedObjectModel {
+    // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
+    if (_managedObjectModel != nil) {
+        return _managedObjectModel;
+    }
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Beck" withExtension:@"momd"];
+    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    return _managedObjectModel;
+}
+
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
+    // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it.
+    if (_persistentStoreCoordinator != nil) {
+        return _persistentStoreCoordinator;
+    }
+    
+    // Create the coordinator and store
+    
+    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"Beck.sqlite"];
+    NSError *error = nil;
+    NSString *failureReason = @"There was an error creating or loading the application's saved data.";
+    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+        // Report any error we got.
+        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+        dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
+        dict[NSLocalizedFailureReasonErrorKey] = failureReason;
+        dict[NSUnderlyingErrorKey] = error;
+        error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
+        // Replace this with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }
+    
+    return _persistentStoreCoordinator;
+}
+
+
+- (NSManagedObjectContext *)managedObjectContext {
+    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.)
+    if (_managedObjectContext != nil) {
+        return _managedObjectContext;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (!coordinator) {
+        return nil;
+    }
+    _managedObjectContext = [[NSManagedObjectContext alloc] init];
+    [_managedObjectContext setPersistentStoreCoordinator:coordinator];
+    return _managedObjectContext;
+}
+
+#pragma mark - Core Data Saving support
+
+- (void)saveContext {
+    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+    if (managedObjectContext != nil) {
+        NSError *error = nil;
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+    }
 }
 
 @end
