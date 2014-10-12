5e6418bf59a07509f4b64e4311f9074a526299a8
Jiang li <jiangli2@yhd.com>
2014-10-12 11:27:55 +0800

    修改

---
+++ Beck/Homepage/AnswerCVC.h
@@ -0,0 +1,13 @@
+//
+//  AnswerCVC.h
+//  Beck
+//
+//  Created by Aimy on 10/12/14.
+//  Copyright (c) 2014 Aimy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnswerCVC : UICollectionViewController
+
+@end
--- Beck/Homepage/ItemTVC.m
+++ Beck/Homepage/ItemTVC.m
@@ -27,20 +27,47 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 10;
+    return 9 + 4;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SubjectCell" forIndexPath:indexPath];
-    
-    cell.textLabel.text = @(indexPath.row).stringValue;
-    
+    UITableViewCell *cell;
+    if (indexPath.row == 0) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"SubjectCell" forIndexPath:indexPath];
+        cell.textLabel.text = @"多项选择题：由一个题干和ABCDE五个备选答案组成，题干在前，选项在后，要求考生从五个备选答案种选出两个或者两个以上的正确答案，多选，少选，错选均不得分";
+    }
+    else if (indexPath.row == 1){
+        cell = [tableView dequeueReusableCellWithIdentifier:@"QuestionCell" forIndexPath:indexPath];
+        cell.textLabel.text = @"医疗机构药剂人员调配处方时的错误行为是";
+    }
+    else if (indexPath.row <= 6){
+        cell = [tableView dequeueReusableCellWithIdentifier:@"OptionCell" forIndexPath:indexPath];
+        cell.textLabel.text = @"我是选项";
+    }
+    else if (indexPath.row == 7){
+        cell = [tableView dequeueReusableCellWithIdentifier:@"ButtonCell" forIndexPath:indexPath];
+    }
+    else if (indexPath.row == 8){
+        cell = [tableView dequeueReusableCellWithIdentifier:@"AnswerCell" forIndexPath:indexPath];
+        cell.textLabel.text = @"我是答案";
+    }
+    else {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"MoreCell" forIndexPath:indexPath];
+    }
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (IBAction)wrongItem:(id)sender {
+    
+}
+
+- (IBAction)addNote:(id)sender {
+    
 }
 
 /*
