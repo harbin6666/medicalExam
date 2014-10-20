43dc2a93d6d8a30623a4b1ed8949cf0d31fee8e7
Jiang li <jiangli2@yhd.com>
2014-10-20 21:36:28 +0800

    修改

---
+++ Beck/More/ChooseExamTVC.m
@@ -0,0 +1,71 @@
+//
+//  ChooseExamTVC.m
+//  Beck
+//
+//  Created by Aimy on 14/10/20.
+//  Copyright (c) 2014年 Aimy. All rights reserved.
+//
+
+#import "ChooseExamTVC.h"
+
+@interface ChooseExamTVC ()
+
+@property (nonatomic, strong) NSArray *names;
+
+@end
+
+@implementation ChooseExamTVC
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    self.names = @[@[@"药理学", @"药物分析", @"药剂学", @"药物化学"],@[@"药理学", @"药物分析", @"药剂学", @"药物化学"]];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UILabel *lbl = [UILabel autolayoutView];
+    lbl.frame = CGRectMake(0, 0, 320, 44);
+    
+    if (section == 0) {
+        lbl.text = @"职业药师(中药)";
+    }
+    else {
+        lbl.text = @"职业药师(西药)";
+    }
+    
+    return lbl;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSArray *secondNames = self.names[section];
+    return secondNames.count;;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    NSArray *secondNames = self.names[indexPath.section];
+    cell.textLabel.text = secondNames[indexPath.row];
+    
+    return cell;
+}
+
+@end
