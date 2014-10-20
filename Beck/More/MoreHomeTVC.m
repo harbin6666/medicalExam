902ebd1f7813ff574b2cc8b11a728d125b38397d
Jiang li <jiangli2@yhd.com>
2014-10-20 21:36:48 +0800

    修改

---
+++ Beck/More/MoreHomeTVC.m
@@ -0,0 +1,81 @@
+//
+//  MoreHomeTVC.m
+//  Beck
+//
+//  Created by Aimy on 14/10/20.
+//  Copyright (c) 2014年 Aimy. All rights reserved.
+//
+
+#import "MoreHomeTVC.h"
+
+@interface MoreHomeTVC ()
+
+@property (nonatomic, strong) NSArray *names;
+
+@end
+
+@implementation MoreHomeTVC
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    self.names = @[@"高频考点", @"考试科目", @"意见反馈", @"使用帮助", @"软件分享", @"版本更新", @"个人档案"];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.names.count;;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = self.names[indexPath.row];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    if (indexPath.row == 0) {
+        [self performSegueWithIdentifier:@"toTestingCentre" sender:nil];
+    }
+    else if (indexPath.row == 1){
+        [self performSegueWithIdentifier:@"toChooseExam" sender:nil];
+    }
+    else if (indexPath.row == 2){
+        [self performSegueWithIdentifier:@"toChooseQuestionBank" sender:nil];
+    }
+    else if (indexPath.row == 3){
+        [self performSegueWithIdentifier:@"toChooseQuestionBank" sender:nil];
+    }
+    else if (indexPath.row == 4){
+        [self performSegueWithIdentifier:@"toChooseQuestionBank" sender:nil];
+    }
+    else if (indexPath.row == 5){
+        [self performSegueWithIdentifier:@"toChooseQuestionBank" sender:nil];
+    }
+    else {
+        [self performSegueWithIdentifier:@"toChooseQuestionBank" sender:nil];
+    }
+}
+
+@end
