910e4f82d050771f761394835cd9a92cbfcbe071
Jiang li <jiangli2@yhd.com>
2014-10-22 21:00:27 +0800

    修改

--- Beck/Homepage/ItemTVC.m
+++ Beck/Homepage/ItemTVC.m
@@ -23,37 +23,71 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return 7;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 9 + 4;
+    if (section == 2) {
+        return 4;
+    }
+    
+    return 1;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section == 0) {
+        return 100.f;
+    }
+    else if (indexPath.section == 1){
+        return 60.f;
+    }
+    else if (indexPath.section == 2){
+        return 30.f;
+    }
+    else if (indexPath.section == 3){
+        return 30.f;
+    }
+    else if (indexPath.section == 4){
+        return 80.f;
+    }
+    else if (indexPath.section == 5){
+        return 80.f;
+    }
+    else {
+        return 44.f;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell;
-    if (indexPath.row == 0) {
+    if (indexPath.section == 0) {
         cell = [tableView dequeueReusableCellWithIdentifier:@"SubjectCell" forIndexPath:indexPath];
         cell.textLabel.text = @"多项选择题：由一个题干和ABCDE五个备选答案组成，题干在前，选项在后，要求考生从五个备选答案种选出两个或者两个以上的正确答案，多选，少选，错选均不得分";
     }
-    else if (indexPath.row == 1){
+    else if (indexPath.section == 1){
         cell = [tableView dequeueReusableCellWithIdentifier:@"QuestionCell" forIndexPath:indexPath];
         cell.textLabel.text = @"医疗机构药剂人员调配处方时的错误行为是";
     }
-    else if (indexPath.row <= 6){
+    else if (indexPath.section == 2){
         cell = [tableView dequeueReusableCellWithIdentifier:@"OptionCell" forIndexPath:indexPath];
         cell.textLabel.text = @"我是选项";
     }
-    else if (indexPath.row == 7){
+    else if (indexPath.section == 3){
         cell = [tableView dequeueReusableCellWithIdentifier:@"ButtonCell" forIndexPath:indexPath];
     }
-    else if (indexPath.row == 8){
+    else if (indexPath.section == 4){
         cell = [tableView dequeueReusableCellWithIdentifier:@"AnswerCell" forIndexPath:indexPath];
-        cell.textLabel.text = @"我是答案";
+        cell.detailTextLabel.text = @"我是答案";
+    }
+    else if (indexPath.section == 5){
+        cell = [tableView dequeueReusableCellWithIdentifier:@"NoteCell" forIndexPath:indexPath];
+        cell.detailTextLabel.text = @"我是笔记";
     }
     else {
         cell = [tableView dequeueReusableCellWithIdentifier:@"MoreCell" forIndexPath:indexPath];
     }
+    
     return cell;
 }
 
