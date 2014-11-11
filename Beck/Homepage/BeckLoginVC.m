08d9100f7410c4ff505860d0df52fe9d62928f3a
Jiang li <322137@qq.com>
2014-11-11 13:19:39 +0800

    update

--- Beck/Homepage/BeckLoginVC.m
+++ Beck/Homepage/BeckLoginVC.m
@@ -38,6 +38,9 @@
     //不设置则获取默认权限
 //    [RennClient setScope:@"read_user_blog read_user_photo read_user_status read_user_album read_user_comment read_user_share publish_blog publish_share send_notification photo_upload status_update create_album publish_comment publish_feed operate_like"];
 }
+- (IBAction)onPressedCheckBox:(UIButton *)sender {
+    sender.selected = !sender.selected;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
