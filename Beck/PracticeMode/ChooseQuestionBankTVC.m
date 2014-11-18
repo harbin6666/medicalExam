ab1542a8733275488eed506e69ff1ba65eea5e6e
Jiang li <322137@qq.com>
2014-11-18 21:04:08 +0800

    update

--- Beck/PracticeMode/ChooseQuestionBankTVC.m
+++ Beck/PracticeMode/ChooseQuestionBankTVC.m
@@ -14,6 +14,8 @@
 
 @property (nonatomic, strong) NSArray *questionBanks;
 
+@property (nonatomic, strong) NSMutableArray *numbers;
+
 @end
 
 @implementation ChooseQuestionBankTVC
@@ -23,17 +25,25 @@
     
     self.tableView.tableFooterView = [UIView new];
     
-    self.questionBanks = @[@[@"药事管理与法规", @"药理学", @"药物分析", @"药剂学", @"药物化学", @"药学综合知识与技能"], @[@"药事管理与法规", @"药理学", @"药物分析", @"药剂学", @"药物化学", @"药学综合知识与技能"]];
+    self.questionBanks = @[@"药事管理与法规", @"药理学", @"药物分析", @"药剂学", @"药物化学", @"药学综合知识与技能"];
     
-    [[AFSQLManager sharedManager] performQuery:@"select lib_id, lib_name from question_library" withBlock:^(NSArray *row, NSError *error, BOOL finished) {
+    self.numbers = @[].mutableCopy;
+    
+    [[AFSQLManager sharedManager] performQuery:@"select a.exam_subject, (select count(b.exam_subject) from question_library as b where b.exam_subject == a.exam_subject) from question_library as a" withBlock:^(NSArray *row, NSError *error, BOOL finished) {
         NSLog(@"%@,%@,%d",row,error,finished);
+        if (finished) {
+            [self.tableView reloadData];
+        }
+        else {
+            [self.numbers addObject:row[1]];
+        }
     }];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 2;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -49,22 +59,22 @@
 {
     UILabel *lbl = [UILabel viewWithFrame:CGRectMake(10, 0, 300, 30)];
     lbl.backgroundColor = [UIColor lightGrayColor];
-    if (section == 0) {
-        lbl.text = @"   章节复习题";
-    }
-    else {
-        lbl.text = @"   综合练习";
-    }
+    lbl.text = @"   章节复习题";
     return lbl;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
     
-    NSArray *names = self.questionBanks[indexPath.section];
+    cell.textLabel.text = self.questionBanks[indexPath.row];
     
-    cell.textLabel.text = names[indexPath.row];
-    
+    NSString *num = self.numbers[indexPath.row];
+    if (num) {
+        cell.detailTextLabel.text = num;
+    }
+    else {
+        cell.detailTextLabel.text = nil;
+    }
     return cell;
 }
 
