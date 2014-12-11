04f47b57ecc74da05b8a76537c0c6890b1dfb1c2
Jiang li <322137@qq.com>
2014-12-11 21:41:43 +0800

    update

--- Beck/MyAccount/Notes/NotesTVC.m
+++ Beck/MyAccount/Notes/NotesTVC.m
@@ -8,11 +8,14 @@
 
 #import "NotesTVC.h"
 
+#import "ItemVO.h"
+#import "ViewItemPVC.h"
+
 @interface NotesTVC ()
 
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 
-@property (nonatomic, strong) NSArray *notes;
+@property (nonatomic, strong) NSArray *items;
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 
 @end
@@ -45,17 +48,16 @@
     [self showLoading];
     
     NSMutableDictionary *params = @{@"loginName":[[NSUserDefaults standardUserDefaults] stringForKey:@"loginName"]}.mutableCopy;
+    params[@"subjectId"] = [[NSUserDefaults standardUserDefaults] valueForKey:@"subjectId"];
     
     if (sender.selectedSegmentIndex == 0) {
         params[@"token"] = @"subject";
     }
     else if (sender.selectedSegmentIndex == 1) {
         params[@"token"] = @"type";
-        params[@"subjectId"] = [[NSUserDefaults standardUserDefaults] valueForKey:@"subjectId"];
     }
     else {
         params[@"token"] = @"outline";
-        params[@"subjectId"] = [[NSUserDefaults standardUserDefaults] valueForKey:@"subjectId"];
     }
     
     WEAK_SELF;
@@ -68,7 +70,7 @@
                 [[OTSAlertView alertWithMessage:aResponseObject[@"msg"] andCompleteBlock:nil] show];
             }
             else {
-                self.notes = aResponseObject[@"list"];
+                self.items = aResponseObject[@"list"];
                 [self.tableView reloadData];
             }
         }
@@ -85,13 +87,13 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.notes.count;
+    return self.items.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"NotesCell" forIndexPath:indexPath];
     
-    NSDictionary *note = self.notes[indexPath.row];
+    NSDictionary *note = self.items[indexPath.row];
     
     if (self.segmentedControl.selectedSegmentIndex == 0) {
         cell.textLabel.text = note[@"subjectName"];
@@ -106,6 +108,60 @@
     cell.detailTextLabel.text = note[@"count"];
     
     return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary *item = self.items[indexPath.row];
+    
+    NSMutableDictionary *params = @{@"loginName":[[NSUserDefaults standardUserDefaults] stringForKey:@"loginName"]}.mutableCopy;
+    params[@"subjectId"] = [[NSUserDefaults standardUserDefaults] valueForKey:@"subjectId"];
+    
+    if (self.segmentedControl.selectedSegmentIndex == 0) {
+        params[@"token"] = @"subjectList";
+    }
+    else if (self.segmentedControl.selectedSegmentIndex == 1) {
+        params[@"token"] = @"outlineList";
+        params[@"outlineId"] = item[@"outlineId"];
+    }
+    else if (self.segmentedControl.selectedSegmentIndex == 2) {
+        params[@"token"] = @"typeList";
+        params[@"type"] = item[@"customId"];;
+    }
+    
+    [self showLoading];
+    WEAK_SELF;
+    [self getValueWithBeckUrl:@"/front/userNoteAct.htm" params:params CompleteBlock:^(id aResponseObject, NSError *anError) {
+        STRONG_SELF;
+        if (!anError) {
+            NSNumber *errorcode = aResponseObject[@"errorcode"];
+            if (errorcode.boolValue) {
+                [[OTSAlertView alertWithMessage:aResponseObject[@"msg"] andCompleteBlock:nil] show];
+            }
+            else {
+                NSMutableArray *ids = [NSMutableArray array];
+                [[aResponseObject[@"list"] firstObject][@"titleList"] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+                    NSDictionary *infos = obj;
+                    ItemVO *itemVO = [ItemVO createWithItemId:[infos[@"titleId"] stringValue] andType:[infos[@"type"] intValue]];
+                    [ids addObject:itemVO];
+                }];
+                
+                if (ids.count > 0) {
+                    [self performSegueWithIdentifier:@"toNext" sender:ids];
+                }
+            }
+        }
+        else {
+            [[OTSAlertView alertWithMessage:@"获取收藏列表失败" andCompleteBlock:nil] show];
+        }
+        [self hideLoading];
+    }];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    ViewItemPVC *vc = segue.destinationViewController;
+    vc.items = sender;
 }
 
 @end
