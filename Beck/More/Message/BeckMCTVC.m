f72b54fb1df9da557bf26054daa369dd3090b1a4
Jiang li <322137@qq.com>
2014-12-30 20:23:28 +0800

    修改

--- Beck/More/Message/BeckMCTVC.m
+++ Beck/More/Message/BeckMCTVC.m
@@ -20,7 +20,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.names = @[@"发现新版本"];
+
     [self showLoading];
     WEAK_SELF;
     [self getValueWithBeckUrl:@"/front/bulletinAct.htm" params:@{@"loginName":[[NSUserDefaults standardUserDefaults] stringForKey:@"loginName"], @"token":@"message"} CompleteBlock:^(id aResponseObject, NSError *anError) {
